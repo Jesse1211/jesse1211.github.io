f698d1a017f7806957a6bb91d44c9d4bbd7ceed4
Jesse Liu <zl942@cornell.edu>
2024-03-31 14:17:29 -0400

    chore: coding style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { FC } from "react"
 import { Home } from "./components/Home"
-import Words from "./components/Words"
 
 export const App : FC = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<Home />} />
-                <Route path = "test" element={<Words />} />
             </Routes>
         </BrowserRouter>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { CssBaseline, CssVarsProvider } from "@mui/joy"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CssVarsProvider>
+				<CssBaseline disableColorScheme />
+				<App />
+    </CssVarsProvider>
   </React.StrictMode>
 )
