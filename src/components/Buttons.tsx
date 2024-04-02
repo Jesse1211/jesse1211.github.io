ec122ebabc5cf2ba1f2be918969d6c97ae728933
Jesse Liu <lzhliu@ucdavis.edu>
2024-04-01 20:44:05 -0400

    chore: aggregate useNavigate hook

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,10 +1,20 @@
 import { forwardRef } from "react";
 import { Button, Stack } from "@mui/joy";
+import { useNavigate } from "react-router-dom";
 
-export const Buttons = forwardRef<HTMLDivElement, {handleCategoryClick : (category: string) => void  }>(
-  ({handleCategoryClick}, ref) => {
+export const Buttons = forwardRef<HTMLDivElement, {isHome: boolean }>(
+  ({isHome}, ref) => {
+    const navigate = useNavigate();
+
+    const handleCategoryClick = (category: string) => {
+      navigate(`/${category}`);
+    };
+    
     return (
       <Stack direction="row" spacing={6} ref={ref}>
+        {isHome && <Button onClick={() => handleCategoryClick("")} style={{ color: "#889def" }} variant="plain">
+          Home
+        </Button>}
         <Button onClick={() => handleCategoryClick("Educations")} style={{ color: "#889def" }} variant="plain">
           Educations
         </Button>
