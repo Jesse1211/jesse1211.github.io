cdecee2ef46cc0da970043c92034f31ab8c8c4cc
Jesse Liu <zl942@cornell.edu>
2024-04-05 16:51:37 -0400

    chore: comment unused file

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,26 +1,26 @@
-import { Stack } from "@mui/joy";
-import { FC } from "react";
-import Fade from "@mui/material/Fade";
-import { Buttons } from "./Buttons";
+// import { Stack } from "@mui/joy";
+// import { FC } from "react";
+// import Fade from "@mui/material/Fade";
+// import { Buttons } from "./Buttons";
 
-export const Navigation: FC = () => {
+// export const Navigation: FC = () => {
 
-  return (
-    <Fade in={location.pathname !== "/"}>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        p={1}
-        style={{
-          position: "fixed",
-          top: 0,
-          width: "100%",
-          textAlign: "center",
-          background: "green",
-        }}
-      >
-        <Buttons isHome={true} />
-      </Stack>
-    </Fade>
-  );
-};
+//   return (
+//     <Fade in={location.pathname !== "/"}>
+//       <Stack
+//         direction="row"
+//         justifyContent="center"
+//         p={1}
+//         style={{
+//           position: "fixed",
+//           top: 0,
+//           width: "100%",
+//           textAlign: "center",
+//           background: "green",
+//         }}
+//       >
+//         <Buttons isHome={true} />
+//       </Stack>
+//     </Fade>
+//   );
+// };
