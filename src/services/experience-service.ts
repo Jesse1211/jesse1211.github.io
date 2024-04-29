bddd3bc0e87b2cdbbba734f276923eb619c04571
Jesse Liu <lzhliu@ucdavis.edu>
2024-04-29 00:27:21 -0400

    env: update environment variable

--- src/services/experience-service.ts
+++ src/services/experience-service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Experience } from "../models/Categories";
 
 const client = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   //   headers: {
   //     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMjE2MzgzNiwianRpIjoiMmJmODE1NDktMDRmMi00NzExLWE2ZWYtNjc2NDZiYWUwMWUzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6bnVsbCwibmJmIjoxNzEyMTYzODM2LCJjc3JmIjoiZDM0ZDUzOGQtYjE4YS00YTZiLWFlMGYtOWFkZWExZjIyZjQyIiwiZXhwIjoxNzEyMTY0NzM2fQ.1W6zOnmbP8vZ6UhgzvIbvGFxG92llkMm5bcgPYWQVX4"
   //   },
