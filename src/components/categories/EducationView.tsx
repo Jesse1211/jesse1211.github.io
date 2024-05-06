0987e6a29a97b9a76cdf29321e966fb88bb8b9b2
Jesse Liu <lzhliu@ucdavis.edu>
2024-05-06 00:50:23 -0400

    chore: add hard coding

--- src/components/categories/EducationView.tsx
+++ src/components/categories/EducationView.tsx
@@ -16,54 +16,65 @@ export const EducationView: FC<{
   responseEducation: Education[];
 }> = ({ responseEducation }) => {
   return (
-    <Stack direction={"row"} flexWrap={"wrap"} gap={4} justifyContent={"center"} overflow={"auto"} >
+    <Stack
+      direction={"row"}
+      flexWrap={"wrap"}
+      gap={4}
+      justifyContent={"center"}
+      overflow={"auto"}
+    >
       {responseEducation.map((education: Education, index) => (
         <CardContainer
           cardView={
-              <Card key={index} sx={{ height: 1 }} size="lg" variant="soft">
-                <AspectRatio ratio="2">
-                  <CardOverflow>
-                    <CardCover>
-                      <img src="./Cornell.jpg" loading="lazy" />
-                    </CardCover>
-                    <CardContent>
-                      <Typography level="body-md" fontWeight="lg">
-                        {education.School}
-                      </Typography>
-                      <Typography level="body-sm" fontWeight="lg">
-                        Master of Engineer
-                      </Typography>
-                    </CardContent>
-                  </CardOverflow>
-                </AspectRatio>
-
-                <CardContent>
-                  {education.ReleventCourses.length > 0 && (
-                    <>
-                      <Typography level="body-sm" fontWeight="lg">
-                        Relevent Courses:
-                      </Typography>
-                      {education.ReleventCourses.map((course, index) => (
-                        <Typography level="body-sm" fontWeight="md" key={index}>
-                          {course}
-                        </Typography>
-                      ))}
-                    </>
-                  )}
-                </CardContent>
-
+            <Card key={index} sx={{ height: 1 }} size="lg" variant="soft">
+              <AspectRatio ratio="2">
                 <CardOverflow>
-                  <Divider inset="context" />
-                  <Typography level="body-sm" fontWeight="md">
-                    GPA: {education.Grade} / 4.0
-                  </Typography>
-                  <Divider orientation="vertical" />
-
-                  <Typography level="body-sm" fontWeight="lg">
-                    {education.StartDate} - {education.EndDate}
-                  </Typography>
+                  <CardCover style={{ display: "contents" }}>
+                    <img
+                      src={education.Image}
+                      loading="lazy"
+                      style={{ width: "auto" }}
+                    />
+                  </CardCover>
                 </CardOverflow>
-              </Card>
+              </AspectRatio>
+
+              <CardContent sx={{"align-items": "center"}}>
+                <Typography level="body-md" fontWeight="lg">
+                  {education.School}
+                </Typography>
+                <Typography level="body-sm" fontWeight="lg">
+                  {education.Major}
+                </Typography>
+              </CardContent>
+
+              <CardContent>
+                {education.Expertise.length > 0 && (
+                  <>
+                    <Typography level="body-sm" fontWeight="lg">
+                    👩🏻‍🔬 Expertise:
+                    </Typography>
+                    {education.Expertise.map((course, index) => (
+                      <Typography level="body-sm" fontWeight="md" key={index}>
+                        {course}
+                      </Typography>
+                    ))}
+                  </>
+                )}
+              </CardContent>
+
+              <CardOverflow>
+                <Divider inset="context" />
+                <Typography level="body-sm" fontWeight="md">
+                  GPA: {education.Grade}
+                </Typography>
+                <Divider orientation="vertical" />
+
+                <Typography level="body-sm" fontWeight="lg">
+                  {education.StartDate} - {education.EndDate}
+                </Typography>
+              </CardOverflow>
+            </Card>
           }
         />
       ))}
