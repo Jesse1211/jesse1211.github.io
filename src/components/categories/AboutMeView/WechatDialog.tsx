97cc42e1007be1d6badca2d30d41e6ef83643e2f
Jesse Liu <zl942@cornell.edu>
2026-06-11 10:20:15 +0800

    feat(modals,about): glass-terminal DescriptionModal, AboutMe, Wechat

--- src/components/categories/AboutMeView/WechatDialog.tsx
+++ src/components/categories/AboutMeView/WechatDialog.tsx
@@ -1,39 +1,45 @@
-import {
-  AspectRatio,
-  Modal,
-  ModalClose,
-  ModalDialog,
-  Typography,
-} from "@mui/joy";
+import { FC } from "react";
+import { Box, Modal, ModalClose, ModalDialog } from "@mui/joy";
+import { GlassPanel } from "../../terminal";
 
-interface WechatModalProps {
+interface Props {
   open: boolean;
   onClose: () => void;
 }
 
-const WechatDialog = (props: WechatModalProps) => {
-  const { open, onClose } = props;
-
-  return (
-    <Modal open={open} onClose={onClose}>
-      <ModalDialog variant="plain" sx={{ gap: "1rem" }}>
-        <ModalClose />
-        <Typography level="title-lg" textColor="inherit">
-          SCAN TO ADD ME
-        </Typography>
-        <AspectRatio
-          ratio={3 / 4}
-          variant="plain"
-          sx={() => ({
-            minWidth: "240px",
-            borderRadius: 20,
-          })}
-        >
-          <img src={`Wechat.jpg`} alt="Wechat QR Code" />
-        </AspectRatio>
-      </ModalDialog>
-    </Modal>
-  );
-};
+const WechatDialog: FC<Props> = ({ open, onClose }) => (
+  <Modal open={open} onClose={onClose}>
+    <ModalDialog
+      variant="plain"
+      sx={{
+        p: 0,
+        background: "transparent",
+        border: "none",
+        boxShadow: "none",
+        maxWidth: "min(360px, 92vw)",
+      }}
+    >
+      <GlassPanel title="$ qrcode wechat.png" glow="active">
+        <ModalClose sx={{ color: "inherit" }} />
+        <Box sx={{ textAlign: "center", py: 1 }}>
+          <Box className="term-accent" sx={{ mb: 1, letterSpacing: "2px" }}>
+            SCAN TO ADD ME
+          </Box>
+          <Box
+            component="img"
+            src="Wechat.jpg"
+            alt="Wechat QR Code"
+            sx={{
+              width: "100%",
+              maxWidth: 240,
+              borderRadius: 1,
+              border: "1px solid hsla(180,100%,70%,0.4)",
+            }}
+          />
+        </Box>
+      </GlassPanel>
+    </ModalDialog>
+  </Modal>
+);
 
 export default WechatDialog;
