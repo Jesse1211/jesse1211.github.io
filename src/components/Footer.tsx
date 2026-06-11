284a3b4f3f90f8dc8a9e4f9a708c50022d726f2f
Jesse Liu <lzhliu@ucdavis.edu>
2026-06-11 10:04:40 +0800

    feat(footer): flow-layout terminal comment footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,20 @@
-import { Stack, Typography } from "@mui/joy";
+import { Box } from "@mui/joy";
 import { FC } from "react";
 
-export const Footer: FC = () => {
-  return (
-    <Stack component="footer" position={"fixed"} bottom={0}>
-      <Typography level="body-xs">© Created by Jesse Liu</Typography>
-    </Stack>
-  );
-};
+export const Footer: FC = () => (
+  <Box
+    component="footer"
+    sx={{
+      width: "100%",
+      textAlign: "center",
+      py: 2,
+      mt: 4,
+      fontSize: 12,
+      fontStyle: "italic",
+      color: "hsla(180,30%,85%,0.55)",
+      fontFamily: "inherit",
+    }}
+  >
+    {`// © 2018-${new Date().getFullYear()} Jesse Liu  ::  built with Vite + zdog`}
+  </Box>
+);
